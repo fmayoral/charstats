3766931d06bf3ca042d7dcba3065e7f23adf2eda
Gaspar Zaragoza <gaspar.zaragoza@niwa.co.nz>
2015-05-18 10:49:08 +1200

    Refactor de habilidades y sus efectos

--- tests/mocha/client/efectosTest.js
+++ tests/mocha/client/efectosTest.js
@@ -3,16 +3,16 @@ if (!(typeof MochaWeb === 'undefined')){
     describe("Efecto - Extra Attack", function(){
       it("should give (n) extra attack", function(){
         var attackRequest = [2,3];
-        var attackResponse = CharStats.efectos.extraAttack(attackRequest, 2);
-        chai.assert.equal(attackResponse.length, 4);
+        var attackResponse = CharStats.efectos.ataque_extra({'ataques':attackRequest}, 2);
+        chai.assert.equal(attackResponse.ataques.length, 4);
 
         var attackRequest = [2];
-        var attackResponse = CharStats.efectos.extraAttack(attackRequest, 1);
-        chai.assert.equal(attackResponse.length, 2);
+        var attackResponse = CharStats.efectos.ataque_extra({'ataques':attackRequest}, 1);
+        chai.assert.equal(attackResponse.ataques.length, 2);
 
         var attackRequest = [2,3];
-        var attackResponse = CharStats.efectos.extraAttack(attackRequest, 5);
-        chai.assert.equal(attackResponse.length, 7);
+        var attackResponse = CharStats.efectos.ataque_extra({'ataques':attackRequest}, 5);
+        chai.assert.equal(attackResponse.ataques.length, 7);
       });
     });
   });
@@ -21,16 +21,16 @@ if (!(typeof MochaWeb === 'undefined')){
     describe("Efecto - Modifica Bonus de Ataque", function(){
       it("should give (n) bonus to all attacks", function(){
         var attackRequest = [2, 3];
-        var attackResponse = CharStats.efectos.modAttackBonus(attackRequest, 2);
-        chai.assert.deepEqual(attackResponse, [4, 5]);
+        var attackResponse = CharStats.efectos.ataque({'ataques':attackRequest}, 2);
+        chai.assert.deepEqual(attackResponse.ataques, [4, 5]);
 
         var attackRequest = [2];
-        var attackResponse = CharStats.efectos.modAttackBonus(attackRequest, 1);
-        chai.assert.deepEqual(attackResponse, [3]);
+        var attackResponse = CharStats.efectos.ataque({'ataques':attackRequest}, 1);
+        chai.assert.deepEqual(attackResponse.ataques, [3]);
 
         var attackRequest = [2, 3];
-        var attackResponse = CharStats.efectos.modAttackBonus(attackRequest, -1);
-        chai.assert.deepEqual(attackResponse, [1, 2]);
+        var attackResponse = CharStats.efectos.ataque({'ataques':attackRequest}, -1);
+        chai.assert.deepEqual(attackResponse.ataques, [1, 2]);
       });
     });
   });
@@ -40,19 +40,19 @@ if (!(typeof MochaWeb === 'undefined')){
       it("should give (n) bonus to all damages", function(){
         var weaponRequest =[{'otro_atributo': false,'bonificador': 2 },{'otro_atributo': false,'bonificador': 0 }];
         var weaponExpectedResponse =[{'otro_atributo': false,'bonificador': 4 },{'otro_atributo': false,'bonificador': 2 }];
-        var weaponResponse = CharStats.efectos.modDamageBonus(weaponRequest, 2);
-        chai.assert.deepEqual(weaponResponse, weaponExpectedResponse);
+        var weaponResponse = CharStats.efectos.danio_bonificador({'weapons':weaponRequest}, 2);
+        chai.assert.deepEqual(weaponResponse.weapons, weaponExpectedResponse);
 
 
         var weaponRequest =[{'otro_atributo': false,'bonificador': 1 }];
         var weaponExpectedResponse =[{'otro_atributo': false,'bonificador': 2 }];
-        var weaponResponse = CharStats.efectos.modDamageBonus(weaponRequest, 1);
-        chai.assert.deepEqual(weaponResponse, weaponExpectedResponse);
+        var weaponResponse = CharStats.efectos.danio_bonificador({'weapons':weaponRequest}, 1);
+        chai.assert.deepEqual(weaponResponse.weapons, weaponExpectedResponse);
 
         var weaponRequest =[{'otro_atributo': false,'bonificador': 0 },{'otro_atributo': false,'bonificador': 2 }];
         var weaponExpectedResponse =[{'otro_atributo': false,'bonificador': -1 },{'otro_atributo': false,'bonificador': 1 }];
-        var weaponResponse = CharStats.efectos.modDamageBonus(weaponRequest, -1);
-        chai.assert.deepEqual(weaponResponse, weaponExpectedResponse);
+        var weaponResponse = CharStats.efectos.danio_bonificador({'weapons':weaponRequest}, -1);
+        chai.assert.deepEqual(weaponResponse.weapons, weaponExpectedResponse);
       });
     });
   });
